ba157c4048f3487ce449a3fa2b4948bde6d62ca5
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-19 22:29:55 -0400

    Addded function to sum numbers from a string

--- src/addStrings.js
+++ src/addStrings.js
@@ -2,19 +2,26 @@ function addstrings(str){
     if (str==""){
         return 0;
     }
-    if(sumStr(str)>0){
-        return sumStr(str);
+    if(findSum(str)>0){
+        return findSum(str);
     }
     return parseInt(str);
 }
 
-function sumStr(str){
-    let strArr = str.split(",");
-    let sum = strArr.reduce(function(total, num){
-      return parseFloat(total) + parseFloat(num);
-    });
-
-    return sum;
+function findSum(str)
+{
+    let temp = "0";
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i];
+        if (!isNaN(String(ch) * 1))
+            temp += ch;
+        else {
+            sum += parseInt(temp);
+            temp = "0";
+        }
+    }
+    return sum + parseInt(temp);
 }
 
- export default addstrings;
+export default addstrings;
--- src/strCalculator.spec.js
+++ src/strCalculator.spec.js
@@ -7,12 +7,22 @@ describe("Add strings", () => {
     });
 
     it("Should return the input string number", () => {
-        const result= addstrings("4");
-        expect(result).toEqual("4");
-      });
+      const result= addstrings("4");
+      expect(result).toEqual(4);
+    });
     
-      it("Should return the addition of numbers from the string", () => {
-        const result= addstrings("4,8");
-        expect(result).toEqual(12);
-      });
+    it("Should return the addition of numbers from the string", () => {
+      const result= addstrings("4,8");
+      expect(result).toEqual(12);
+    });
+
+    it("Should return the addition of numbers from the string", () => {
+      const result= addstrings("4,8,12");
+      expect(result).toEqual(24);
+    });
+
+    it("Should return the addition of numbers from the string even when we have multiple characters", () => {
+      const result= addstrings("4,8,12-24");
+      expect(result).toEqual(48);
+    });
   });
