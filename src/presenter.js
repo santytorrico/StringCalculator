2ac67da69b902830788bc770e0f52368494b12da
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-19 23:44:40 -0400

    -m "added interface"

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,12 @@
-import sumar from "./sumador";
+import addstrings from "./addStrings.js";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const firstNumber = first.value;
+  div.innerHTML = "<p>" + addstrings(firstNumber) + "</p>";
 });
